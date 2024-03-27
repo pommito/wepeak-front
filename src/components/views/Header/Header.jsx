3247b362666d9d585165e3d28189f9fb069b1597
NicolasChambon <nico.chambon@gmail.com>
2024-03-27 22:49:32 +0100

    feat: responsive tablet done

--- src/components/views/Header/Header.jsx
+++ src/components/views/Header/Header.jsx
@@ -7,21 +7,19 @@ import './Header.scss';
 const Header = () => {
   return (
     <header className="Header">
-      <div className="Header-left">
-        <img
-          className="Header-left-logo"
-          src="/src/assets/Logo_BW.svg"
-          alt="Logo de WePeak"
-        />
-        <form className="Header-left-form">
-          <input type="text" placeholder="Commune de la sortie" />
-          <button type="submit">
-            <IconContext.Provider value={{ size: '1.2rem' }}>
-              <FiSearch className="search-logo" />
-            </IconContext.Provider>
-          </button>
-        </form>
-      </div>
+      <img
+        className="Header-logo"
+        src="/src/assets/Logo_BW.svg"
+        alt="Logo de WePeak"
+      />
+      <form className="Header-form">
+        <input type="text" placeholder="Ville ou code postal" />
+        <button type="submit">
+          <IconContext.Provider value={{ size: '1.2rem' }}>
+            <FiSearch className="search-logo" />
+          </IconContext.Provider>
+        </button>
+      </form>
       <nav className="Header-nav">
         <ul className="Header-nav-links">
           <li className="Header-nav-link Header-nav-link-connect">
