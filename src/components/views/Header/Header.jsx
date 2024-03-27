572b4dbc49dc84947972d7691f9fc27b509b105b
NicolasChambon <nico.chambon@gmail.com>
2024-03-27 23:54:47 +0100

    fix: bug with logo display in mobile view

--- src/components/views/Header/Header.jsx
+++ src/components/views/Header/Header.jsx
@@ -1,17 +1,21 @@
 import { IconContext } from 'react-icons';
 import { FiSearch } from 'react-icons/fi';
-import { NavLink } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 
 import './Header.scss';
 
 const Header = () => {
   return (
     <header className="Header">
-      <img
-        className="Header-logo"
-        src="/src/assets/Logo_BW.svg"
-        alt="Logo de WePeak"
-      />
+      <Link to="/">
+        <div className="Header-logo-container">
+          <img
+            className="Header-logo"
+            src="/src/assets/Logo_BW.svg"
+            alt="Logo de WePeak"
+          />
+        </div>
+      </Link>
       <form className="Header-form">
         <input type="text" placeholder="Ville ou code postal" />
         <button type="submit">
