1b8ce96479a895b85170cac22cc6c48d9c37701e
NicolasChambon <nico.chambon@gmail.com>
2024-03-27 14:58:42 +0100

    setup: scss & custom font & logo

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,28 +1,13 @@
-import logo from '../../assets/logo.svg';
+import Header from '../views/Header/Header';
 
 import './App.scss';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-
-        <p>
-          Edit <code>src/components/App/App.jsx</code> and save to reload.
-        </p>
-
-        <a
-          className="App-link"
-          href="https://react.dev/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
     </div>
   );
-}
+};
 
 export default App;
