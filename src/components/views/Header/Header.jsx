dced5c0fd2e95da68679061f6d8a31a076fb14df
NicolasChambon <nico.chambon@gmail.com>
2024-03-27 16:05:22 +0100

    wip: header style not finished

--- src/components/views/Header/Header.jsx
+++ src/components/views/Header/Header.jsx
@@ -6,15 +6,29 @@ import './Header.scss';
 const Header = () => {
   return (
     <header className="Header">
-      <img src="/src/assets/Logo_BW.svg" alt="Logo de WePeak" />
-      <form action="">
-        <input type="text" />
-        <button type="submit">
-          <CiSearch />
-        </button>
-      </form>
-      <NavLink to="/login">Se connecter</NavLink>
-      <NavLink to="/register">S&apos;inscrire</NavLink>
+      <div className="Header-left">
+        <img
+          className="Header-left-logo"
+          src="/src/assets/Logo_BW.svg"
+          alt="Logo de WePeak"
+        />
+        <form action="">
+          <input type="text" />
+          <button type="submit">
+            <CiSearch />
+          </button>
+        </form>
+      </div>
+      <nav className="Header-nav">
+        <ul className="Header-nav-links">
+          <li className="Header-nav-link">
+            <NavLink to="/login">Se connecter</NavLink>
+          </li>
+          <li className="Header-nav-link">
+            <NavLink to="/register">S&apos;inscrire</NavLink>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 };
