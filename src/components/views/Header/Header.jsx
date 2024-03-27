5d8e9d48961de0b16bc19aeeb8cee544feaf80ac
NicolasChambon <nico.chambon@gmail.com>
2024-03-28 00:19:25 +0100

    feat: mode connected done

--- src/components/views/Header/Header.jsx
+++ src/components/views/Header/Header.jsx
@@ -1,10 +1,15 @@
 import { IconContext } from 'react-icons';
 import { FiSearch } from 'react-icons/fi';
+import { FaRegMessage } from 'react-icons/fa6';
+import { PiUserCircle } from 'react-icons/pi';
+
 import { NavLink, Link } from 'react-router-dom';
 
 import './Header.scss';
 
 const Header = () => {
+  const isLogged = false;
+
   return (
     <header className="Header">
       <Link to="/">
@@ -25,14 +30,34 @@ const Header = () => {
         </button>
       </form>
       <nav className="Header-nav">
-        <ul className="Header-nav-links">
-          <li className="Header-nav-link Header-nav-link-connect">
-            <NavLink to="/login">Se connecter</NavLink>
-          </li>
-          <li className="Header-nav-link Header-nav-link-register">
-            <NavLink to="/register">S&apos;inscrire</NavLink>
-          </li>
-        </ul>
+        {isLogged && (
+          <ul className="Header-nav-links">
+            <li className="Header-nav-link Header-nav-link-connect">
+              <NavLink to="/login">Se connecter</NavLink>
+            </li>
+            <li className="Header-nav-link Header-nav-link-register">
+              <NavLink to="/register">S&apos;inscrire</NavLink>
+            </li>
+          </ul>
+        )}
+        {!isLogged && (
+          <ul className="Header-nav-links">
+            <li className="Header-nav-link Header-nav-link-messages">
+              <NavLink to="/messages">
+                <IconContext.Provider value={{ size: '2.2rem' }}>
+                  <FaRegMessage />
+                </IconContext.Provider>
+              </NavLink>
+            </li>
+            <li className="Header-nav-link Header-nav-link-profile">
+              <NavLink to="/profile">
+                <IconContext.Provider value={{ size: '3rem' }}>
+                  <PiUserCircle />
+                </IconContext.Provider>
+              </NavLink>
+            </li>
+          </ul>
+        )}
       </nav>
     </header>
   );
