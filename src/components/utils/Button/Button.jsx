7b300947e7536349624ad50f258878abe2b01418
Victor Lebecq <victor.lebecq.pro@gmail.com>
2024-03-28 13:40:42 +0100

    WIP : added prop-types for components

--- src/components/utils/Button/Button.jsx
+++ src/components/utils/Button/Button.jsx
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import './Button.scss';
 
 const Button = ({ text, className, url }) => {
@@ -6,6 +7,12 @@ const Button = ({ text, className, url }) => {
       {text}
     </a>
   );
+};
+
+Button.propTypes = {
+  text: PropTypes.string.isRequired,
+  className: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired,
 };
 
 export default Button;
