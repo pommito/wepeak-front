a7c4af1b90a9ec3e8a84d49b1fa7ccaccd7501e6
NicolasChambon <nico.chambon@gmail.com>
2024-04-08 21:36:52 +0200

    wip

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,4 @@
 import { Route, Routes, useLocation } from 'react-router-dom';
-import { useEffect } from 'react';
 
 import Header from '../views/Header/Header';
 import Footer from '../views/Footer/Footer';
@@ -7,11 +6,9 @@ import Home from '../views/Home/Home';
 import Activity from '../views/Activity/Activity';
 import Contact from '../views/Contact/Contact';
 import Error404 from '../views/Error404/Error404';
-import Footer from '../views/Footer/Footer';
 import Profile from '../views/Profile/Profile';
 import UserPage from '../views/UserPage/UserPage';
 
-import './App.scss';
 import Activities from '../views/Activities/Activities';
 import Login from '../views/Login/Login';
 import Register from '../views/Register/Register';
@@ -33,6 +30,7 @@ const App = () => {
         <Route path="/activities/:slug" element={<Activity />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/user/:slug" element={<UserPage />} />
         <Route path="*" element={<Error404 />} />
